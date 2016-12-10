332c15817e5a3d2bce6c4b2ddce8d41657491838
krzysztofkolek <krzysztof.kolek.praca@gmail.com>
2016-12-10 17:24:52 +0100

    Naming fix

--- NorthwindAPI/AdventureWorksAPI.Tests/Controllers/ValuesControllerTest.cs
+++ NorthwindAPI/AdventureWorksAPI.Tests/Controllers/ValuesControllerTest.cs
@@ -5,10 +5,10 @@ using System.Net.Http;
 using System.Text;
 using System.Web.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NorthwindAPI;
-using NorthwindAPI.Controllers;
+using AdventureWorksAPI;
+using AdventureWorksAPI.Controllers;
 
-namespace NorthwindAPI.Tests.Controllers
+namespace AdventureWorksAPI.Tests.Controllers
 {
     [TestClass]
     public class ValuesControllerTest
